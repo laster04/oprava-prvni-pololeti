6712b8716bcd40ae179793237913e8c0271c4323
lukas.sirhal <e1Sil3487>
2019-11-05 13:30:51 +0100

    udpate tests

--- src/main/java/Vlacek.java
+++ src/main/java/Vlacek.java
@@ -18,13 +18,17 @@ public class Vlacek {
      * Přidávejte vagonky do vlaku
      * Podmínka je že vagonek první třídy musí být vždy řazen za předchozí vagonek toho typu, pokud žádný takový není je řazen rovnou za lokomotivu
      * vagonek 2 třídy musí být vždy řazen až za poslední vagonek třídy první
-     * Poštovní vagonek musí být vždy poslední vagonek lokomotivy
+     * Poštovní vagonek je vždy za posledním vagónkem DRUHE_TRIDY
+     * Vagonky TRETI_TRIDY se vždy řadí na konec vláčku jako poslední vagonky
      * Při vkládání vagonku nezapomeňte vagonku přiřadit danou pozici ve vlaku
      * !!!!!!! POZOR !!!!!! pokud přidáváte vagonek jinak než na konec vlaku musíte všem následujícím vagonkům zvýšit jejich umístění - doporučuji si pro tento účel vytvořit privátní metodu
      * @param type
      */
     public void pridatVagonek(VagonekType type) {
 
+    }
+
+    private void incrementUmisteni(Vagonek vagonek) {
     }
 
     public Vagonek getVagonekByIndex(int index) {
@@ -45,13 +49,15 @@ public class Vlacek {
      * @return
      */
     public Vagonek getLastVagonekByType(VagonekType type) {
-        return null;
+        Vagonek last = lokomotiva;
+        return last;
     }
+
 
     /**
      * Tato funkce přidá jídelní vagonek za poslední vagonek první třídy, pokud jídelní vagonek za vagonkem první třídy již existuje
      * tak se další vagonek přidá nejblíže středu vagonků druhé třídy
-     * tzn: pokud budu mít č osobních vagonků tak zařadím jídelní vagonek za 2 osobní vagónek
+     * tzn: pokud budu mít 4 osobních vagonků tak zařadím jídelní vagonek za 2 osobní vagónek
      * pokud budu mít osobních vagonků 5 zařadím jídelní vagonek za 3 osobní vagonek
      */
     public void pridatJidelniVagonek() {
@@ -64,7 +70,7 @@ public class Vlacek {
      * @param type
      * @return
      */
-    public int getDelkaByType(VagonekType type) {
+    public int getPocetVagonkuByType(VagonekType type) {
         return 0;
     }
 
